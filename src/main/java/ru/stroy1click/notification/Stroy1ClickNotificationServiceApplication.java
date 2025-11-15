46f95a8f9e9b8b6a23ec6e1e8345b17e62467a83
Egor <egormalov.2007@gmail.com>
2025-11-15 21:03:55 +0300

    Initial commit

---
+++ src/main/java/ru/stroy1click/notification/Stroy1ClickNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.stroy1click.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stroy1ClickNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stroy1ClickNotificationServiceApplication.class, args);
+    }
+
+}
